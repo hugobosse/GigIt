6191a45f04516e7430110887e9069c95e00b124c
hugo bosse <monsieurbosse@gmail.com>
2017-02-20 16:25:47 +0100

    added bar model and controller

---
+++ app/controllers/bars_controller.rb
@@ -0,0 +1,19 @@
+class BarsController < ApplicationController
+  before_action :set_bar, only: [:show]
+  def index
+    @bars = Bar.all
+  end
+
+  def show
+    @bar = Bar.find(params[:id])
+  end
+
+  private
+
+  def set_bar
+    @bar = Bar.find(params[:id])
+  end
+
+  def bar_params
+    params.require(:bar).permit(:name, :genre, :address, :price)
+end
