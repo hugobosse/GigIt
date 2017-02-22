7501a6ec03fb2f87b0812271005ddc92797e4ae4
Patrick Gourrier <patgourrier@gmail.com>
2017-02-22 01:41:05 +0100

    La search form affiche les params mais les résultats ne s'affichent pas correctement

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,7 +1,14 @@
 class BarsController < ApplicationController
   before_action :set_bar, only: [:show]
+
+
   def index
     @bars = Bar.all
+    if params[:search]
+      @bars = Bar.where("genre LIKE ?", params["search"["genre"]])
+    else
+      @bars = Bar.all
+    end
   end
 
   def show
