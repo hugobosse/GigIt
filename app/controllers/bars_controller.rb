8d9ff00656a26f15d7629bda45db01e21a55a462
hugo bosse <monsieurbosse@gmail.com>
2017-02-22 09:19:41 +0100

    added booking seed and @last_booked_bars instance

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -2,6 +2,8 @@ class BarsController < ApplicationController
   before_action :set_bar, only: [:show]
   def index
     @bars = Bar.all
+    @last_bookings = Booking.order(booking_date: :desc).map { |booking| booking.bar }
+    @last_booked_bars = @last_bookings.uniq
   end
 
   def show
