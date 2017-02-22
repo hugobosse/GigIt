66a46c97f7985de958be66fc44ea464d3eb91d23
Benjamin Gahery <benjamingahery@gmail.com>
2017-02-22 15:41:47 +0100

    as a logged user i can delete one of my bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,11 @@
 class BookingsController < ApplicationController
+
+  before_action :set_booking, only: [:show, :destroy]
   def index
     @bookings = current_user.bookings
   end
 
   def show
-    @booking = Booking.find(params[:id])
   end
 
   def create
@@ -17,7 +18,16 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 
   def booking_params
     params.require(:booking).permit(:booking_date, :user_id, :bar_id)
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create, :show]
   end
 
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :destroy]
 
 end
