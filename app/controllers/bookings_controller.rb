21348348e3c6cf901ea05618e575b2873654a85b
hugo bosse <monsieurbosse@gmail.com>
2017-02-22 16:01:09 +0100

    started adding add a review feature, I want to pull master to have bookings#index up to date

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,6 +18,18 @@ class BookingsController < ApplicationController
     end
   end
 
+  def add_review
+    @review = Review.new(review_params)
+  # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @bar = bar.find(params[:bar_id])
+    @review.bar = @bar
+    if @review.save
+      redirect_to bookings_path
+    else
+      render :add_review
+    end
+  end
+
   private
 
   def booking_params
